678ed4045daaa0c19b91bbf0ba2339dea4f950b8
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-08-26 02:39:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,11 +8,11 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "20389440"))
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-    FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7043502393:AAEdH1AegBbo9KIA91jWcie7KRE9gXXG5kw")
+    FSUB = getenv("FSUB", "about_tosuu")
+    CHID = int(getenv("CHID", "-1002023182491"))
+    SUDO = list(map(int, getenv("SUDO", "6848223695").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
 
