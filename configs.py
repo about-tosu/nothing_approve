11e28ccb19d09a6085e59c6c33d8562b11b35f41
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-12-08 00:08:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config:
     API_ID = "24620300"
     API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
     BOT_TOKEN = "7043502393:AAEdH1AegBbo9KIA91jWcie7KRE9gXXG5kw"
-    FSUB = bout_tosuu"
+    FSUB = "about_tosuu"
     CHID = "-1002324687097"
     SUDO = "6848223695"
     MONGO_URI = "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
